4f826d4f3fe9812e611ee7979338c8d97a82e83f
Stefan Dragnev <dragnev@telerik.com>
2014-10-10 12:46:55 +0300

    fix build

--- Telerik.JustMock.Tests/Properties/AssemblyInfo.cs
+++ Telerik.JustMock.Tests/Properties/AssemblyInfo.cs
@@ -16,6 +16,7 @@
 */
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
