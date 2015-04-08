cd85c3dbbcad9de18076954416ae044aa1ac8132
Stefan Dragnev <dragnev@telerik.com>
2015-04-08 12:53:22 +0300

    workaround for proxying android types with inaccurate metadata

--- Telerik.JustMock.Portable/MockedTypeAttribute.cs
+++ Telerik.JustMock.Portable/MockedTypeAttribute.cs
@@ -1,20 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Telerik.JustMock
 {
+	/// <summary>
+	/// Declares that the proxy generator must emit a proxy for the given type.
+	/// This attribute is used whenever the proxy generator cannot infer from that
+	/// some type will be proxied.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
 	public sealed class MockedTypeAttribute : Attribute
 	{
+		/// <summary>
+		/// An array of interface types that the proxy will implement additionally.
+		/// </summary>
 		public Type[] Implements { get; set; }
 
+		/// <summary>
+		/// Declares the type for which a proxy must be emitted.
+		/// </summary>
+		/// <param name="mockedType">The type to proxy.</param>
 		public MockedTypeAttribute(Type mockedType)
-		{
-		}
-
-		public MockedTypeAttribute(string mockedTypeName, string assemblyName)
 		{
 		}
 	}
