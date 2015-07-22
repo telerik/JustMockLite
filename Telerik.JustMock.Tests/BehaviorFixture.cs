1e0d9f440866e5cbc0fab22dce037ecdd6b9b49d
Stefan Dragnev <dragnev@telerik.com>
2015-07-22 18:00:43 +0300

    fix silverlight test

--- Telerik.JustMock.Tests/BehaviorFixture.cs
+++ Telerik.JustMock.Tests/BehaviorFixture.cs
@@ -277,7 +277,7 @@ namespace Telerik.JustMock.Tests
 			catch (Exception) { }
 
 			var message = Assert.Throws<AssertionException>(() => Mock.Assert(mock)).Message;
-#if !COREFX
+#if !COREFX || SILVERLIGHT
 			Assert.Equal("Called unarranged member 'Void Invoke()' on strict mock of type 'Castle.Proxies.Delegates.System_Action'", message.Trim());
 #else
 			Assert.Equal("Called unarranged member 'Void Invoke()' on strict mock of type 'Telerik.JustMock.DelegateBackends.System.Action'", message.Trim());
