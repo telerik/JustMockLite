5ef5bde55925929d9e864863d0feaf3525a51e7d
Kamen Ivanov <kamen.ivanov@progress.com>
2017-05-18 13:10:34 +0300

    add non elevated CS examples for VS 2017

---
+++ Examples/CSExamples/JustMock.NonElevatedExamples.VS2017/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JustMock.NonElevatedExamples.VS2017")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JustMock.NonElevatedExamples.VS2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f2e15f1-5a5d-483f-9986-85a8985b7b67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
