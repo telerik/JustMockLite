5640e2262480a84bc00f82c6b366396e1b4630c2
Kamen Ivanov <kamen.ivanov@telerik.com>
2017-07-10 11:12:08 +0300

    add Telerik.JustMock.MSTest2.Test.Elevated to InternalsVisible in DemoLibS

--- Telerik.JustMock.DemoLib/Properties/AssemblyInfo.cs
+++ Telerik.JustMock.DemoLib/Properties/AssemblyInfo.cs
@@ -58,3 +58,4 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo(Telerik.JustMock.DemoLib.JustMock.FullyQualifiedAssemblyName)]
 [assembly: InternalsVisibleTo("Telerik.JustMock.Tests.Elevated")]
 [assembly: InternalsVisibleTo("Telerik.JustMock.MSTest2.Tests")]
+[assembly: InternalsVisibleTo("Telerik.JustMock.MSTest2.Tests.Elevated")]
