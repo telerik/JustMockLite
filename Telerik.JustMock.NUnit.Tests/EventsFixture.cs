8cfd4c00fce43338a1fef85d766dcc4ad95e73be
Lyubomir Rusev <lyubomir.rusev@progress.com>
2018-03-12 14:47:45 +0200

    Reduce code duplication between EventsFixture shared tests and the one that is specific only to NUnit framework.

---
+++ Telerik.JustMock.NUnit.Tests/EventsFixture.cs
@@ -0,0 +1,76 @@
+﻿/*
+ JustMock Lite
+ Copyright © 2010-2015 Telerik AD
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Telerik.JustMock.Core;
+using Telerik.JustMock.Tests.EventFixureDependencies;
+
+#region JustMock Test Attributes
+#if NUNIT
+using NUnit.Framework;
+using TestCategory = NUnit.Framework.CategoryAttribute;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using AssertionException = NUnit.Framework.AssertionException;
+#elif XUNIT
+using Xunit;
+using Telerik.JustMock.XUnit.Test.Attributes;
+using TestCategory = Telerik.JustMock.XUnit.Test.Attributes.XUnitCategoryAttribute;
+using TestClass = Telerik.JustMock.XUnit.Test.Attributes.EmptyTestClassAttribute;
+using TestMethod = Xunit.FactAttribute;
+using TestInitialize = Telerik.JustMock.XUnit.Test.Attributes.EmptyTestInitializeAttribute;
+using TestCleanup = Telerik.JustMock.XUnit.Test.Attributes.EmptyTestCleanupAttribute;
+using AssertionException = Xunit.Sdk.AssertException;
+#elif VSTEST_PORTABLE
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using AssertionException = Microsoft.VisualStudio.TestPlatform.UnitTestFramework.AssertFailedException;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssertionException = Microsoft.VisualStudio.TestTools.UnitTesting.AssertFailedException;
+#endif
+#endregion
+
+namespace Telerik.JustMock.Tests
+{
+	[TestClass]
+	public class EventsFixtureNUnit
+	{
+		private ProjectNavigatorViewModel viewModel;
+		private ISolutionService solutionService;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			this.solutionService = Mock.Create<ISolutionService>();
+			this.viewModel = new ProjectNavigatorViewModel(this.solutionService);
+		}
+
+		[TestMethod, TestCategory("Lite"), TestCategory("Events")]
+		[TestCaseSource("DummyTestCaseSource")]
+		public void ShouldRaiseEventsOnDataDrivenTests(object _)
+		{
+			Mock.Raise(() => this.solutionService.ProjectAdded += null, new ProjectEventArgs(null));
+		}
+
+		private static IEnumerable<TestCaseData> DummyTestCaseSource = new[] { new TestCaseData(null), new TestCaseData(null) };
+	}
+}
---
+++ Telerik.JustMock.Tests/EventsFixtureDependencies.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+
+namespace Telerik.JustMock.Tests.EventFixureDependencies
+{
+	public class FooArgs : EventArgs
+	{
+		public FooArgs()
+		{
+		}
+
+		public FooArgs(string value)
+		{
+			this.Value = value;
+		}
+
+		public string Value { get; set; }
+	}
+
+	public interface IExecutor<T>
+	{
+		event EventHandler<FooArgs> Done;
+		event EventHandler Executed;
+		void Execute(T value);
+		void Execute(string value);
+		void Execute(string s, int i);
+		void Execute(string s, int i, bool b);
+		void Execute(string s, int i, bool b, string v);
+
+		string Echo(string s);
+	}
+
+	public class Foo
+	{
+
+	}
+
+	public interface IFoo
+	{
+		event CustomEvent CustomEvent;
+		event EchoEvent EchoEvent;
+		void RaiseMethod();
+		string Echo(string arg);
+		void Execute();
+	}
+
+	public class ProjectNavigatorViewModel
+	{
+		public ProjectNavigatorViewModel(ISolutionService solutionService)
+		{
+			this.SolutionService = solutionService;
+			SolutionService.ProjectAdded += new EventHandler<ProjectEventArgs>(SolutionService_ProjectAdded);
+		}
+
+		void SolutionService_ProjectAdded(object sender, ProjectEventArgs e)
+		{
+			IsProjectAddedCalled = true;
+		}
+
+		public ISolutionService SolutionService { get; set; }
+		public bool IsProjectAddedCalled { get; set; }
+	}
+
+	public class ProjectEventArgs : EventArgs
+	{
+		private IFoo foo;
+
+		public ProjectEventArgs(IFoo foo)
+		{
+			this.foo = foo;
+		}
+	}
+
+	public interface ISolutionService
+	{
+		event EventHandler<ProjectEventArgs> ProjectAdded;
+	}
+
+	public delegate void CustomEvent(string value);
+	public delegate void EchoEvent(bool echoed);
+
+	public class SolutionService : ISolutionService
+	{
+		public event EventHandler<ProjectEventArgs> ProjectAdded;
+	}
+}
