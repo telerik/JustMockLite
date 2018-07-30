bd08216639744dc30b9ffc819cc69a8695b5408e
Ivo Stoilov <istoilov@progress.com>
2018-07-30 14:58:04 +0300

    Support for C# 4 Named parameters
    o added new Arrange overload for actions

--- Telerik.JustMock/Helpers/MultipleReturnValueChainHelper.cs
+++ Telerik.JustMock/Helpers/MultipleReturnValueChainHelper.cs
@@ -67,7 +67,7 @@ namespace Telerik.JustMock.Helpers
 				{
 					var methodReturnType = callPattern.Method.GetReturnType();
 					var returnTypeMessage = methodReturnType.IsAssignableFrom(typeof(TReturn))
-						? String.Format("The arranged function is not set up to return a value of type {0}.  If this is a non-public arrangement then make sure that the call to Arrange specifies the correct return type, e.g. Mock.NonPublic.Arrange<int>(...) if the method returns 'int'.", typeof(TReturn))
+						? String.Format("The arranged function is not set up to return a value of type {0}, please make sure that the call to Arrange specifies the correct return type, e.g. Mock.Arrange<int>(...) for instance and Mock.Arrange<Foo, int>(...) for static methods if the return type is 'int'. If this is a non-public arrangement then use the corresponding overload from Mock.NonPublic API", typeof(TReturn))
 						: String.Format("The chained return value type '{0}' is not compatible with the arranged method's return type '{1}'", typeof(TReturn), methodReturnType);
 
 					throw new MockException(returnTypeMessage);
